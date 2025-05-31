afd13ceecec1a8bad9cc5aa8b6fa538ca8c6fb4b
rozwer <your.email@example.com>
2025-05-31 19:00:01 +0900

    リリース準備2

--- tasks/release.js
+++ tasks/release.js
@@ -13,11 +13,11 @@ const version = packageJSON.version;
  * -----------------------------------------------------------------
  * Modified by: roz
  * Date       : 2025-05-28
- * Changes    : リリースファイル名をcomfortable-sakaiからcomfortable-ishiiに変更
+ * Changes    : リリースファイル名をcomfortable-sakaiからcomfortable-NUに変更
  * Category   : リリース設定
  * -----------------------------------------------------------------
  */
-const output = fs.createWriteStream(`./dist/release/comfortable-ishii-${browser}-v${version}.zip`);
+const output = fs.createWriteStream(`./dist/release/comfortable-NU-${browser}-v${version}.zip`);
 const archive = archiver("zip", {
     zlib: { level: 9 }
 });
@@ -34,11 +34,11 @@ switch (browser) {
          * -----------------------------------------------------------------
          * Modified by: roz
          * Date       : 2025-05-28
-         * Changes    : Chromeリリース用ディレクトリ名をcomfortable-sakaiからcomfortable-ishiiに変更
+         * Changes    : Chromeリリース用ディレクトリ名をcomfortable-sakaiからcomfortable-NUに変更
          * Category   : リリース設定
          * -----------------------------------------------------------------
          */
-        archive.directory(`./dist/source/${browser}`, `comfortable-ishii-v${version}`);
+        archive.directory(`./dist/source/${browser}`, `comfortable-NU-v${version}`);
         break;
     case "firefox":
         archive.directory(`./dist/source/${browser}`, ``);
